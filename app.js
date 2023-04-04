f5903a6f032b7d64b2ee8d61611fd7292022f2a3
Jason McGrath <jnomad21@yahoo.com>
2023-04-03 19:05:15 -0700

    JM Port 1.4

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 document.querySelector(".back-video").playbackRate=1;
 document.querySelector(".raindrops").playbackRate=.8;
+
 const entryPage = document.querySelector('#drop')
 const entryButton = document.querySelector('#drop-button')
-entryButton.addEventListener("click", toggleEntry)
+const rezButton = document.querySelector('rezButton')
+const rez = document.querySelector("#resume-show")
+
+entryButton.addEventListener("click", toggleEntryDrop)
+rezButton.addEventListener("click", toggleEntryShow)
 
 
-function toggleEntry(){
+function toggleEntryDrop(){
 // entryPage.classList.toggle('#drop-close')
 entryPage.style.display="none"
+}
+function toggleEntryShow(){
+rez.style.display="block"
 }
