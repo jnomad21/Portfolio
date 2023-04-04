ccd80ede1235f0b5874c7b28bae74f8250000262
Jason McGrath <jnomad21@yahoo.com>
2023-04-03 18:16:16 -0700

    stage 3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.querySelector(".back-video").playbackRate=1;
+document.querySelector(".raindrops").playbackRate=.8;
 const entryPage = document.querySelector('#drop')
 const entryButton = document.querySelector('#drop-button')
 entryButton.addEventListener("click", toggleEntry)
